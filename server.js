1f6d1281184d23aa4fc242d3f9789a2c001f5506
KennRobot <kebrgonzalezdo@ittepic.edu.mx>
2025-05-06 11:22:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ module.exports = async (o) => {
     app.use(express.json({limit:'500kb'})); 
     app.use(cors()); 
     app.use('/api', router); 
-    app.get('/', (req, res) => { 
+    /* app.get('/', (req, res) => { 
     res.end('SAP CDS está en ejecución...${req.url}');
-    }); 
+    }); */ 
     o.app = app; 
     o.app.httpServer = await cds.server(o); 
     } catch (error) { 
